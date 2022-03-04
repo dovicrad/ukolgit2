23765f48d511a11a53cad25a6121aa720c3e63bc
dovicrad <dovicrad@fel.cvut.cz>
2022-03-04 22:42:12 +0100

    making tests

--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -1,2 +1,13 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class FactorialTest {
+    @Test
+    public void factorialTest(){
+        Factorial fact = new Factorial();
+        int n = 3;
+        long expectedValue = 6;
+        long result = fact.factorial(n);
+        Assertions.assertEquals(expectedValue, result);
+    }
 }
